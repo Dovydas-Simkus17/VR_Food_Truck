4dc2c2fdd11f5871d45452d2e9ba7c26344947ed
Dovydas-Simkus17 <simkusdovydas0@gmail.com>
2026-02-21 15:48:34 +0000

    Setting Up Cooking, Still Need to Finish Stove

--- Assets/Scripts/Cooking.cs
+++ Assets/Scripts/Cooking.cs
@@ -1,16 +1,29 @@
+using Unity.Collections.LowLevel.Unsafe;
 using UnityEngine;
 
 public class Cooking : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public bool isActive = false;
+    public float heatLevel = 0f;
+    public ParticleSystem fireEffect;
+
+    public void SetHeat(float value)
     {
-        
+        heatLevel = Mathf.Clamp01(value);
+        isActive = heatLevel > 0;
+
+        if (isActive && !fireEffect.isPlaying)
+        {
+            fireEffect.Play();
+        }
+        else if (!isActive && fireEffect.isPlaying)
+        {
+            fireEffect.Stop();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public float GetHeat()
     {
-        
+        return heatLevel;
     }
 }
